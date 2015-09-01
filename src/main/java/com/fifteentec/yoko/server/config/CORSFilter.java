383e636fe3ac7147b1892c40725782b722be0a20
zs263104840 <263104840@qq.com>
2015-09-01 16:02:54 +0800

    update

--- src/main/java/com/fifteentec/yoko/server/config/CORSFilter.java
+++ src/main/java/com/fifteentec/yoko/server/config/CORSFilter.java
@@ -26,9 +26,9 @@ public class CORSFilter implements Filter{
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization, Accept, Content-type, Origin");
 		chain.doFilter(req, res);
 	}
 
